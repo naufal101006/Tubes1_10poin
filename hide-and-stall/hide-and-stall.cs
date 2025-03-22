c0ee9ed1efecbc770eafc8f7dddf86c239dfdfd9
naufal101006 <143169855+naufal101006@users.noreply.github.com>
2025-03-22 12:50:38 +0700

    Hide and Stall: Complete Heuristic Change

--- hide-and-stall/hide-and-stall.cs
+++ hide-and-stall/hide-and-stall.cs
@@ -3,11 +3,11 @@ using System.Drawing;
 using Robocode.TankRoyale.BotApi.Events;
 using System.Collections.Generic;
 using System;
-using System.Linq;
 
 public class HideAndStall : Bot
 {
     public Dictionary<int, double[]> RobotList { get; private set; }
+    public double[][] Grid { get; private set; }
 
     // The main method starts our bot
     static void Main(string[] args)
@@ -37,49 +37,19 @@ public class HideAndStall : Bot
 
         Console.WriteLine($"{ArenaWidth}, {ArenaHeight}");
 
-        static double GetN(double[][] Grid, int i, int j, int ic, int jc) {
-            if (i+ic < 0 || i+ic >= Grid.Length || j+jc < 0 || j+jc >= Grid[0].Length) {
-                return Grid[i][j];
-            } else {
-                return Grid[i+ic][j+jc];
-            };
-        }
-
         TurnRadarLeft(360);
         while (IsRunning)
         {
             SetTurnRadarLeft(360);
+            bool DoJitter = true;
 
-            if (EnemyCount > 1) {
-                double[][] Grid = {
-                    new double[4] {0, 0, 0, 0},
-                    new double[4] {0, 0, 0, 0},
-                    new double[4] {0, 0, 0, 0},
-                    new double[4] {0, 0, 0, 0},
-                };
+            CalculateGrid();
+            int[] CurrentGrid = new int[2] {(int)(X*4/ArenaWidth), (int)(Y*4/ArenaHeight)};
 
-                foreach (var Coords in RobotList)
-                {
-                    Grid[(int)(Coords.Value[1]*4 / ArenaHeight)][(int)(Coords.Value[0]*4 / ArenaWidth)] += 1;
-                }
-                
-                for (int k = 0; k < 3; k++) {
-                    for (int i = 0; i < Grid.Length; i++)
-                    {
-                        for (int j = 0; j < Grid[0].Length; j++)
-                        {
-                            Grid[i][j] += (
-                                GetN(Grid, i, j, -1, -1) + GetN(Grid, i, j, -1, 0) + GetN(Grid, i, j, -1, 1)
-                                + GetN(Grid, i, j, 0, -1) + 0 + GetN(Grid, i, j, 0, 1)
-                                + GetN(Grid, i, j, 1, -1) + GetN(Grid, i, j, 1, 0) + GetN(Grid, i, j, 1, 1)
-                            )/8.0f;
-                        }
-                    }
-                }
-
+            if (Grid[CurrentGrid[0]][CurrentGrid[1]] > 0.4) {
                 double Minimum = 9999;
-                const double THRESHOLD = 0.4;
                 double[] GridCoords = new double[2] {0, 0};
+
                 for (int i = 0; i < Grid.Length; i++) {
                     for (int j = 0; j < Grid[0].Length; j++) {
                         if (Grid[i][j] < Minimum) {
@@ -87,9 +57,6 @@ public class HideAndStall : Bot
                             GridCoords[1] = ((double)i + 1.0f/2.0f) * ArenaHeight/8;
 
                             Minimum = Grid[i][j];
-                            if (Minimum < THRESHOLD) {
-                                break;
-                            }
                         }
                     }
                 }
@@ -97,6 +64,8 @@ public class HideAndStall : Bot
                 double AngleToPoint = BearingTo(GridCoords[0], GridCoords[1]);
                 double DistanceToPoint = DistanceTo(GridCoords[0], GridCoords[1]);
                 if (LastTarget[0] != GridCoords[0] || LastTarget[1] != GridCoords[1] || DistanceToPoint > 50) {
+                    DoJitter = false;
+
                     if (AngleToPoint > 0) {
                         SetTurnLeft(AngleToPoint);
                     } else if (AngleToPoint < 0) {
@@ -111,13 +80,13 @@ public class HideAndStall : Bot
 
                     LastTarget[0] = GridCoords[0];
                     LastTarget[1] = GridCoords[1];
-                } else {
-                    // Jitter
-                    SetTurnLeft(RNG.NextDouble()*360);
-                    Forward((RNG.Next(0, 2)*2-1) * RNG.Next(1, 3)*18);
-                    Go();
                 }
-                Console.WriteLine($"Going to {GridCoords[0]}, {GridCoords[1]}");
+            }
+            
+            if (DoJitter) {
+                SetTurnLeft(RNG.NextDouble()*360);
+                Forward((RNG.Next(0, 2)*2-1) * RNG.Next(1, 3)*18);
+                Go();
             }
         }
     }
@@ -130,7 +99,44 @@ public class HideAndStall : Bot
 
     public override void OnHitBot(HitBotEvent e)
     {
-        SetTurnLeft(Direction % 90);
+        TurnLeft(BearingTo(e.X, e.Y) + 90);
+        Forward(64);
+    }
+
+    private void CalculateGrid() {
+        Grid = new double[4][] {
+            new double[4] {0, 0, 0, 0},
+            new double[4] {0, 0, 0, 0},
+            new double[4] {0, 0, 0, 0},
+            new double[4] {0, 0, 0, 0},
+        };
+
+        foreach (var Coords in RobotList)
+        {
+            Grid[(int)(Coords.Value[1]*4 / ArenaHeight)][(int)(Coords.Value[0]*4 / ArenaWidth)] += 1;
+        }
+        
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < Grid.Length; i++)
+            {
+                for (int j = 0; j < Grid[0].Length; j++)
+                {
+                    Grid[i][j] += (
+                        GetN(Grid, i, j, -1, -1) + GetN(Grid, i, j, -1, 0) + GetN(Grid, i, j, -1, 1)
+                        + GetN(Grid, i, j, 0, -1) + 0 + GetN(Grid, i, j, 0, 1)
+                        + GetN(Grid, i, j, 1, -1) + GetN(Grid, i, j, 1, 0) + GetN(Grid, i, j, 1, 1)
+                    )/8.0f;
+                }
+            }
+        }
+    }
+
+    static double GetN(double[][] Grid, int i, int j, int ic, int jc) {
+        if (i+ic < 0 || i+ic >= Grid.Length || j+jc < 0 || j+jc >= Grid[0].Length) {
+            return Grid[i][j];
+        } else {
+            return Grid[i+ic][j+jc];
+        };
     }
 }
 
